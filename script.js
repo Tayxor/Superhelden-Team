5b5dd20c715c437d30ce079bca1fe580fad3e2e8
Leon Müller <l.mueller@ynfinite.com>
2022-08-09 20:32:06 +0200

    added count

--- script.js
+++ script.js
@@ -54,14 +54,14 @@ window.onload = function(){
         readTextFile(file, function(text){
             var data = JSON.parse(text);
             const member = data.messages[Math.floor(Math.random()*data.messages.length)].content            
-            console.log(file.replace('.json', '') + ' ' + data.messageCount)
+            // console.log(file.replace('.json', '') + ' ' + data.messageCount)
             
             if (member.length > 0){
                 for (let i = 0; i < 5; i++) {
                     animationTeam.push(data.messages[Math.floor(Math.random()*data.messages.length)].content)                    
                 }
 
-                document.querySelector('#' + file.replace('.json', '')).innerHTML = member
+                document.querySelector('#' + file.replace('.json', '')).innerHTML = member  + ' (' + data.messageCount + ')'
             } else{
                 renderTeam()
             }
